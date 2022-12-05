18ae64e8c7ef5a95c2d9b71921fb375e79f3f665
tunguyen <tunguyenkawa@gmail.com>
2022-12-06 06:58:40 +0700

    add project

---
+++ src/test/java/com/nbstech/spring/basic/movierecommendersystem/MovierecommendersystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nbstech.spring.basic.movierecommendersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovierecommendersystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
