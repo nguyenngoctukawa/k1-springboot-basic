5728a5239b1ad8e64c6bd041eaa0e47ece2a1072
tunguyen <tunguyenkawa@gmail.com>
2022-12-14 07:40:26 +0700

    Config Spring app manually without Spring Boot

--- src/main/java/com/nbstech/spring/basic/movierecommendersystem/MovieRecommenderSystemApplication.java
+++ src/main/java/com/nbstech/spring/basic/movierecommendersystem/MovieRecommenderSystemApplication.java
@@ -4,17 +4,22 @@ import com.nbstech.spring.basic.movierecommendersystem.Lesson3.MovieRecommender;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 
 import java.util.Arrays;
 
-@SpringBootApplication
+//@SpringBootApplication
+@ComponentScan
 public class MovieRecommenderSystemApplication {
 
 	public static void main(String[] args) {
 		//SpringApplication.run(MovieRecommenderSystemApplication.class, args);
-		ApplicationContext appContext= SpringApplication.run(MovieRecommenderSystemApplication.class, args);
+//		ApplicationContext appContext= SpringApplication.run(MovieRecommenderSystemApplication.class, args);
 		//create object of RecommenderImplementation class
+		AnnotationConfigApplicationContext appContext =
+				new AnnotationConfigApplicationContext(MovieRecommenderSystemApplication.class);
 		MovieRecommender recommender = appContext.getBean(MovieRecommender.class);
 
 
@@ -24,6 +29,7 @@ public class MovieRecommenderSystemApplication {
 
 		//display results
 		System.out.println(Arrays.toString(result));
+		appContext.close();
 	}
 
 }
